4d47fbda854d9e486769d5c02c4eb3bd038f9127
Rose <ebrue7949@gmail.com>
2022-10-16 01:49:51 +0300

    new

--- src/components/Main/cards.jsx
+++ src/components/Main/cards.jsx
@@ -6,7 +6,7 @@ const Card = (data) => {
       <div className="title">
         <h1>{title}</h1>
       </div>
-      <img src={image} alt="" />
+
       <div className="card-over">
         <p>{desc}</p>
       </div>
